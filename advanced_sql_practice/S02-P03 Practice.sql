6a03a8b158bab4f164684d82e522695ca66de09f
jessicazhu44 <48077797+jessicazhu44@users.noreply.github.com>
2019-08-05 13:47:19 +0800

    Update S02-P03 Practice.sql

--- advanced_sql_practice/S02-P03 Practice.sql
+++ advanced_sql_practice/S02-P03 Practice.sql
@@ -10,24 +10,24 @@ INSERT INTO `bi_data`.`valid_offers`
 		price_usd, 
 		original_price, 
 		original_currency_code,
-    	checkin_date, 
-    	checkout_date, 
-    	breakfast_included_flag, 
-    	valid_from_date, 
-    	valid_to_date
-	)
-;
+    		checkin_date, 
+    		checkout_date, 
+    		breakfast_included_flag, 
+    		valid_from_date, 
+    		valid_to_date
+		)
+	;
 
 SELECT 
 	`offer_cleanse_date_fix`.`id`, 
 	`offer_cleanse_date_fix`.`hotel_id`, 
 	`offer_cleanse_date_fix`.`sellings_price` AS `offer_selling_price_usd`, 
-    `lst_currency`.`code` AS `lst_currency_code`, 
-    `offer_cleanse_date_fix`.`checkin_date`, 
-    `offer_cleanse_date_fix`.`checkout_date`, 
-    `offer_cleanse_date_fix`.`breakfast_included_flag`,
-    `offer_cleanse_date_fix`.`offer_valid_from`, 
-    `offer_cleanse_date_fix`.`offer_valid_to`
+    	`lst_currency`.`code` AS `lst_currency_code`, 
+    	`offer_cleanse_date_fix`.`checkin_date`, 
+    	`offer_cleanse_date_fix`.`checkout_date`, 
+    	`offer_cleanse_date_fix`.`breakfast_included_flag`,
+    	`offer_cleanse_date_fix`.`offer_valid_from`, 
+    	`offer_cleanse_date_fix`.`offer_valid_to`
 FROM 1=1,
 	 `enterprise_data`.`offer_cleanse_date_fix`, 
 	 `primary_data`.`lst_currency`
